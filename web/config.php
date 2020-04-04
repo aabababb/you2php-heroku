911a0dd6ff1dd4253e52c98e1955ea4d7f943ea3
aabababb <471352702@qq.com>
2020-04-04 17:35:03 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', getenv("AIzaSyDXkyQpEODmf7JIS4q4BZsLqjB-HzCTF2E"));
+define('GJ_CODE', getenv("CN"));
+define('SITE_NAME',getenv("haose888.net"));
+define('TITLENAME', getenv("haose888.net"));
+define('EN2DEKEY',getenv("1234abcd"));
+define('EMAIL', getenv("klwx888a@gmail.com"));
 ?>
